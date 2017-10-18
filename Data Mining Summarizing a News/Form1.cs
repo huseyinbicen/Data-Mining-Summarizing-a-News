670bb090cb5beec328caa77503e06fb7c812c864
Hüseyin Biçen <huseynbicen@outlook.com>
2017-10-18 03:12:35 +0300

    master

--- Data Mining Summarizing a News/Form1.cs
+++ Data Mining Summarizing a News/Form1.cs
@@ -12,14 +12,30 @@ namespace Data_Mining_Summarizing_a_News
 {
     public partial class Form1 : Form
     {
+        String makele = "";
+        String ozet = "";
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            txt_Ozet.Clear();
 
+            makele = txt_Makale.Text;
+            char[] karakter = { '.', ',', ' ', ':',';','!', '+' , '-','*','/','&','%','(',')','?','{','}','[',']','<','>','|'};
+            List<String> dizi = makele.Split(karakter).ToList();
+
+            foreach (String item in dizi)
+            {
+                ozet += item + " ";
+            }
+
+            txt_Ozet.Text = ozet ;
         }
+
+      
     }
 }
